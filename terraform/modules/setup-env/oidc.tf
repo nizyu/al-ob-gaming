680704a97493c87a6bfdcee1b801d10b87713d61
Shogo Takaki <en19926@gmail.com>
2024-08-17 17:14:45 +0900

    restart (#8)
    * iam
    
    * upgrade
    
    * fix

--- terraform/modules/setup-env/oidc.tf
+++ terraform/modules/setup-env/oidc.tf
@@ -35,8 +35,13 @@ resource "aws_iam_role" "github_actions_role" {
 }
 
 
-## TODO: 権限つけすぎ問題。
 resource "aws_iam_role_policy_attachment" "tfc_policy_attachment" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/AWSLambda_FullAccess",
+    "arn:aws:iam::aws:policy/IAMReadOnlyAccess",
+    "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess",
+  ])
   role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
+  policy_arn = each.value
 }
